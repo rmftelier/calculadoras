611dff2763c948638b312ab7deb80f9ec853cab1
ROBERTA MEYRELLES FRANÇA <roberta.franca@uvvnet.com.br>
2023-11-28 20:49:40 -0300

    código inicial + estilização

---
+++ js/calculadora.js
@@ -0,0 +1,20 @@
+/* 
+1. Crie variáveis que capturem os valores que a usuária inserir e manipule-os nas funções 
+2. Exibir o resultado na tela
+*/
+
+function somar() {
+
+}
+
+function subtrair() {
+
+}
+
+function multiplicar() {
+
+}
+
+function dividir() {
+
+}
